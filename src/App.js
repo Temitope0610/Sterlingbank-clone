8fdfc0b5ab9e8a3d5a4883983c92b990500c3a81
temitope soyoye <herbarbie06@gmail.com>
2021-03-08 16:41:13 +0100

    main comparison

--- src/App.js
+++ src/App.js
@@ -4,26 +4,18 @@ import Home from './pages/home';
 import './App.css';
 import '../node_modules/bootstrap';
 import '../node_modules/animate.css'
-import Home from './pages/home'
 import Personal from './pages/personal';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-       <Router>
-         <Switch>
-           <Route path='/' exact><Home /></Route>
-           <Route path='/personal'><Personal /></Route>
-         </Switch>
-       </Router> */}
         <Router>
             <Switch>
                 <Route path ="/" exact ><Home/></Route>
                 <Route path='/personal'><Personal /></Route>
             </Switch>
         </Router>
-       </Router>
     </div>
   );
 }
