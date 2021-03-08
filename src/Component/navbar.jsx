f453ac8c2dcda31488b86bbd1d446d9d1bbc84a1
temitope soyoye <herbarbie06@gmail.com>
2021-03-08 09:15:20 +0100

    changes made in herobox css

--- src/Component/navbar.jsx
+++ src/Component/navbar.jsx
@@ -5,17 +5,17 @@ class Navbar extends React.Component{
     render(){
         return(
             <div>
-                <div className='navbar'>
+                <div className='her0-navbar'>
                     <div className='container'>
                         <div className='row'>
                             <div className='col-2'>
-                                <ul className='nav-menu float-start p-3'>
+                                <ul className='hero-nav-menu float-start p-3'>
                                    <a href='/personal'><li className='personal-list'>Personal</li></a>
                                     <Link to=''><li>Business</li></Link>
                                 </ul>
                             </div>
                             <div className='col-10'>
-                                <ul className='nav-menu float-end p-3'>
+                                <ul className='hero-nav-menu float-end p-3'>
                                     <li>Investors</li>
                                     <li>Non-Interest</li>
                                     <li>Private</li>
