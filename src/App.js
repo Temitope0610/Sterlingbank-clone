59d35a44b61c16d12d2939ce360b4dbd4bb4c71e
temitope soyoye <herbarbie06@gmail.com>
2021-03-08 09:26:01 +0100

    changes made in duplicate css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Home from './Component/home'
-import './App.css';
+// import './App.css';
 import '../node_modules/bootstrap';
 import Icon from './Component/icon'
 import '../node_modules/animate.css'
 import Personal from './Component/personal';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
@@ -13,12 +13,12 @@ function App() {
        {/* <Home />
        <Icon />
        <Personal /> */}
-       <Router>
+       {/* <Router>
          <Switch>
            <Route path='/' exact><Home /></Route>
            <Route path='/personal'><Personal /></Route>
          </Switch>
-       </Router>
+       </Router> */}
     </div>
   );
 }
